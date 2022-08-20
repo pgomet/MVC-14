664bf9189854c4ccda465b3507c2f429a4d093e6
Pina Gomet <101363048+pgomet@users.noreply.github.com>
2022-08-19 22:20:49 -0400

    populate the template with sequalize data

---
+++ controllers/index.js
@@ -0,0 +1,6 @@
+const router = require('./home-routes.js');
+const homeRoutes = require('./home-routes.js');
+
+router.use('/', homeRoutes);
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const path = require('path');
 
+const routes = require('./controllers/');
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, 'public')));
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
